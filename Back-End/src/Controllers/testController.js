e2c488ef169367b7a4672e9b87fc022e0727918e
Karthick-2335 <karthick2335@gmail.com>
2025-09-20 07:58:59 +0530

    karthick

--- Back-End/src/Controllers/testController.js
+++ Back-End/src/Controllers/testController.js
@@ -2,5 +2,5 @@ import test from '../Models/test.js'
 
 export async function checking (req,res) {
   var findTestResult = await test.find()
-  res.json(findTestResult)  ;
-};
+  res.json(findTestResult) ;
+}; 
